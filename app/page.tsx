510b7e5dcab44611f7fd85a1cc629210873bf2fc
Cassio-Sales <cassiososales@gmail.com>
2025-03-26 08:41:37 +0000

    add dynamic import to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Grid from "@/components/Grid";
+//import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
+//import RecentProjects from "@/components/RecentProjects";
 import Approach from "@/components/ui/Approach";
 import Clients from "@/components/ui/Clients";
 import Experience from "@/components/ui/Experience";
@@ -9,10 +9,16 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Footer from "@/components/ui/Footer";
 import { navItems } from "@/data";
 
+import dynamic from 'next/dynamic'
 
-if (typeof window !== "undefined") {
-  
-}
+const RecentProjects = dynamic(() => import('../components/RecentProjects'), {
+  ssr: false
+})
+
+const Grid = dynamic(() => import('../components/Grid'), {
+  ssr: false
+})
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
