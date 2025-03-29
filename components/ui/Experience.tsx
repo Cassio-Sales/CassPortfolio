9748350dec2b9a9e24dd77756e1fa8a4c740291c
Cassio-Sales <cassiososales@gmail.com>
2025-03-29 12:42:36 +0000

    create new section for certificate and add photos

--- components/ui/Experience.tsx
+++ components/ui/Experience.tsx
@@ -2,37 +2,46 @@ import { workExperience } from '@/data'
 import React from 'react'
 import { Button } from './MovingBorders'
 
-if (typeof window !== "undefined") {
-  require("typeface-inter")
+if (typeof window !== 'undefined') {
+  require('typeface-inter')
 }
+
 const Experience = () => {
   return (
     <div className="py-20" id="tetimonials">
       <h1 className="heading">
-        My
-        <span className="text-purple"> work experience</span>
+        
+        <span className="text-purple"> Certificates</span>
       </h1>
-      <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
-        {workExperience.map((card) => (
+      <div className="w-full mt-12 grid sm:grid-cols-2 lg:grid-cols-4 grid-cols-1 gap-10">
+        {workExperience.map(card => (
           <Button
             key={card.id}
-            duration={Math.floor(Math.random() * 10000 + 10000 )}
-            borderRadius='1.75rem'
+            duration={Math.floor(Math.random() * 10000 + 10000)}
+            borderRadius="2rem"
             className="flex-1 text-white border-neutral-200 dark:border-slate-800"
           >
-            <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
-              <img src={card.thumbnail} alt={card.thumbnail} className="lg:w-32 md:w-20 w-16" />
-              <div className="lg:ms-5">
-                <h1 className="text-start text-xl md:text-2xl font-bold">
+            <div className="relative w-full pb-[60%]">
+              {' '}
+              {/* Diminuindo mais pela metade */}
+              <img
+                src={card.thumbnail}
+                alt={card.thumbnail}
+                className="absolute inset-0 w-full h-full object-cover rounded-lg"
+              />
+              <div className="absolute inset-0 flex flex-col items-center justify-center p-4">
+                <h1 className="text-sm font-bold text-white text-center">
+                  {' '}
+                  {/* Fonte mantida */}
                   {card.title}
                 </h1>
-                <p className="text-start text-white-100 mt-3 font-semibold">
+                <p className="mt-2 text-white text-xs font-semibold text-center">
+                  {' '}
+                  {/* Texto mantido */}
                   {card.desc}
                 </p>
               </div>
-
             </div>
-              
           </Button>
         ))}
       </div>
