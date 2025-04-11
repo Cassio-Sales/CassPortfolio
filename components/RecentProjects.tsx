5bc8444143e92c4ce2d215c4d086d9a45750130f
Cassio-Sales <cassiososales@gmail.com>
2025-04-11 14:22:51 +0100

    fix recent projects img responsiveness

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -3,6 +3,7 @@ import { projects } from '@/data'
 import { PinContainer } from './ui/3d-pin'
 import { FaLocationArrow } from 'react-icons/fa'
 import { TypewriterEffect } from './ui/Typewriter'
+
 const RecentProjects = () => {
   return (
     <div className="py-20" id="projects">
@@ -15,12 +16,6 @@ const RecentProjects = () => {
         ]}
       />
 
-      {/* 
-<h1 className="heading">
-  A small selection of{' '}
-  <span className="text-purple">recent projects</span>
-</h1> 
-*/}
       <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <a
@@ -28,30 +23,35 @@ const RecentProjects = () => {
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw] lg:[h-30vh] mb-10"
+            className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw] mb-10"
           >
             <PinContainer title={link} href={link}>
-              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[40vh] mb-10">
-                {/* Ajuste para ocupar o container todo */}
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+              {/* Container da imagem ajustado para altura da imagem no mobile */}
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-auto mb-10">
+                {/* Imagem de fundo */}
+                <div className="absolute inset-0 w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d] z-0">
                   <img
                     src="/bg.png"
                     alt="bg-img"
                     className="w-full h-full object-cover"
                   />
                 </div>
+
+                {/* Imagem do projeto */}
                 <img
                   src={img}
                   alt={title}
-                  className="z-10 absolute bottom-0 w-full h-full object-cover"
+                  className="z-10 relative w-full h-auto object-contain sm:absolute sm:inset-0 sm:h-full sm:object-cover sm:object-center"
                 />
               </div>
+
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {title}
               </h1>
               <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
                 {des}
               </p>
+
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
                   {iconLists.map((icon, index) => (
@@ -66,8 +66,7 @@ const RecentProjects = () => {
                 </div>
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-sm text-purple">
-                    {' '}
-                    Check Live Site{' '}
+                    Check Live Site
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
